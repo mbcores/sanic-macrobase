8bff46f19f326368520131bc9fe37922a82e8742
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-02-19 12:42:39 +0500

    use last version driver

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='alexey.shagaleev@yandex.ru',
     description='Sanic driver for macrobase framework',
     install_requires=[
-        'macrobase-driver==0.0.2',
+        'macrobase-driver>=0.0.4',
         'sanic==18.12.0',
         'structlog==19.1.0'
     ]
