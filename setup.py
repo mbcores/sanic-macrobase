4c3851b57bf6fbacb9f21d0bbb3d4e42641e070d
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-02-20 12:05:35 +0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sanic_macrobase',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages(),
     url='https://github.com/mbcores/sanic-macrobase',
     license='MIT',
