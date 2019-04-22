7aee1782992e4b538c44459285eca17652104cdf
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-04-22 13:26:10 +0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sanic_macrobase',
-    version='0.0.9',
+    version='0.0.10',
     packages=find_packages(),
     url='https://github.com/mbcores/sanic-macrobase',
     license='MIT',
@@ -10,7 +10,7 @@ setup(
     author_email='alexey.shagaleev@yandex.ru',
     description='Sanic driver for macrobase framework',
     install_requires=[
-        'macrobase-driver>=0.0.11',
+        'macrobase-driver>=0.0.14',
         'sanic==18.12.0',
         'structlog==19.1.0'
     ]
