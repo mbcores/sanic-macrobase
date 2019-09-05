c9bf6bf5216c09c4cfb175ca76217fed1c80bb94
Alex Sandakov <sandakov.aleksey65@gmail.com>
2019-09-05 13:54:52 +0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sanic_macrobase',
-    version='0.0.13',
+    version='0.0.14',
     packages=find_packages(),
     url='https://github.com/mbcores/sanic-macrobase',
     license='MIT',
